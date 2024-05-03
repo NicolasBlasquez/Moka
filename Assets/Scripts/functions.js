d714d289bb21075e238f4f65413e119c9e813dcb
Nick <blasqueznicolas2006@gmail.com>
2024-05-02 22:53:42 -0300

    Alpha 0.2

--- Assets/Scripts/functions.js
+++ Assets/Scripts/functions.js
@@ -3,8 +3,24 @@
 function GET(url, callback) {
     let xhr = new XMLHttpRequest();
     xhr.addEventListener("load", function () {
-        callback(xhr.response);
+        if (xhr.status == 200) {
+            callback(xhr.response);
+        } else throw new Error(`Failed Connection`);
     });
     xhr.open("GET", url);
     xhr.send(null);
 }
+
+function loadImage(url) {
+    const i = new Image();
+    i.src = url;
+}
+
+function printProduct(product) {
+    let container = Object.assign(document.createElement("div"), { classList: ["product"], innerHTML: `<div class="productName"><h1>${product[0][0]}</h1><h3>${product[0][4]}</h3></div><p>${product[0][5]}</p>` } );
+    return container;
+}
+
+loadImage("Assets/Img/box.svg");
+loadImage("Assets/Img/person.svg");
+loadImage("Assets/Img/house.svg");
